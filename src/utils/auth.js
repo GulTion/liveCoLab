fcd44f0c080942d903ab35062cebe9ed3589eba0
gultion <gkv8192@gmail.com>
2025-01-19 14:33:01 +0530

    push url

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-export const SERVER_URL = localStorage.getItem("server");
+export const SERVER_URL = 'http://192.168.244.12:8080';
 
 // "http://192.168.244.12:8080"
-export const WS_SERVER_URL = localStorage.getItem("wserver");
+export const WS_SERVER_URL = 'http://localhost:5000';
