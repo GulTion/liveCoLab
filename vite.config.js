dd4e14881696dde5152d5e46733256c93bc6fee7
gultion <gkv8192@gmail.com>
2025-01-19 13:35:29 +0530

    push

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/liveCoLab/', // Replace <repository-name> with your GitHub repository name
+
 });
